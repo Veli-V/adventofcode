11d9bd3b2ff9d1c04d441a33408c3c3e52695350
Janne Väisänen <janne.vaisanen@gofore.com>
2018-12-10 10:31:41 +0200

    added day 7 task1 and started task2

--- day7/task1.py
+++ day7/task1.py
@@ -3,10 +3,10 @@ import sys
 import string
 from array import *
 
-f = open('input_test.txt', 'r')
-#f = open('input_task.txt', 'r')
+#f = open('input_test.txt', 'r')
+f = open('input_task.txt', 'r')
 
-height = 6   # For Test 6, for task 26
+height = 26   # For Test 6, for task 26
 width = 1
 ABCCONSTANT = 65
 
@@ -15,25 +15,20 @@ completedSteps = []
 orderArray = [[0 for x in range(width)] for y in range(height)]
 abcList = list(string.ascii_uppercase)
 
-def canBeDone(x):
-  if len(x[1]) == 0:
+def canBeDone(step):
+  if len(step[1]) == 0 and step[0] != 'Done':
     return True
   return False
 
 def completeStep(step):
-  if step == 'Done':
+  #if step == 'Done':
     #print("ollaan tehty jo")
-    return
+  #  return
       
   #print('koitetaan tehdä: ' + step)
   i = 0
   stepInt = ord(step)-ABCCONSTANT
   end = len(orderArray[stepInt][1])
-  while i < end:
-    #print(step + ' Tarvii ainakin: ' + orderArray[stepInt][1][i])
-    completeStep(orderArray[stepInt][1][i])
-    end -= 1
-    i += 1
 
   #print("tehtiin nyt sit: " + step)
   for x in orderArray:
@@ -65,9 +60,19 @@ for line in f:
 for y in orderArray:
   print(y)
 
-for y in orderArray:
+i = 0
+end = len(orderArray)
+
+while i < end:
+  if canBeDone(orderArray[i]):
+    completeStep(orderArray[i][0])
+    i = 0
+  else:
+    i += 1
+
+#for y in orderArray:
   #print(y)
-  completeStep(y[0])
+#  completeStep(y[0])
   
 
 print('Taskit suoritettiin järjestyksessä: ', end = '') 
