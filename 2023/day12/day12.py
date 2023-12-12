510d6b6600162c18ea40cd4b3e7db058e0a71e39
Veli-V <vaisanenjj@gmail.com>
2023-12-12 22:50:40 +0200

    day 12 in different state

--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -14,46 +14,43 @@ with open(input_file) as f:
 
 data = [d.strip() for d in data]
 
-for d in data:
-    print(d)
-
-def positionsStart(row, numbers):
-    cnum = numbers[0]
-    pos = 0
-    for idc, c, in enumerate(row):
-        if c == "?":
-            for i in range(idc, len(row)):
-                if row[i] != "?":
-                    spotLenght = i-idc
-                    break
-            if spotLenght >= cnum:
-                print(row[:idc] + str(cnum)*cnum + row[idc+cnum:])
-                if len(numbers) > 1:
-                    pos += positions(row[idc+cnum:], numbers[1:], idc+cnum)
-    return pos
-
 def positions(row, numbers, intention):
-    print(" "*intention + "Tarkistetaan: " + row + " numeroilla " + str(numbers))
+    #print(" "*intention + "Tarkistetaan: " + row + " numeroilla " + str(numbers))
     cnum = numbers[0]
     pos = 0
+    spotLenght = 0
     for idc, c, in enumerate(row):
-        if c == "?":
+        if c != ".":
             for i in range(idc, len(row)):
-                if row[i] != "?":
+                if row[i] == ".":
                     spotLenght = i-idc
                     break
+                if i == len(row)-1:
+                    spotLenght = i-idc+1
+                    break
             if spotLenght >= cnum:
-                pos += 1
                 print(" "*intention, end="")
                 print(row[:idc] + str(cnum)*numbers[0] + row[idc+cnum:])
                 if len(numbers) > 1:
-                    pos *= positions(row[idc+cnum:], numbers[1:], intention+idc+cnum)
-    print(" "*intention + "Returning: ", pos)
-    if pos == 0: return 1
+                    pos += positions(row[idc+cnum+1:], numbers[1:], intention+idc+cnum)
+                else:
+                    pos += 1
+    #print(" "*intention + "Returning: ", pos)
     return pos
 
 print()
 print()
-ans = positionsStart(data[1], [2,1,1])
+ans = 0
+tans = 0
+#for d in data:
+    #row = d.split(" ")
+    #print(d, end =": ")
+    #tans = positions(row[0],[int(i) for i in row[1].split(",")], 0)
+    #print(tans)
+    #ans += tans
+
+print(data[2])
+row = data[2].split(" ")
+ans = positions(row[0],[int(i) for i in row[1].split(",")], 0)
 print()
 print("part1:", ans)
